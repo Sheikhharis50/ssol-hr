ebd940d8326920b282587056422c03f53bf762e9
Sheikh Haris Zahid <sheikh.haris.zahid@hotmail.com>
2021-11-14 01:20:02 +0500

    UI fixed

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 type Props = {};
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <Box component="main" sx={{ flexGrow: 1, p: 3, m: "64px 0 0 0" }}>
-      <section className="content">{children}</section>
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Nav />
+      <Box component="main" sx={{ flexGrow: 1, p: 3, m: "64px 0 0 0" }}>
+        <section className="content">{children}</section>
+      </Box>
+      <Footer />
     </Box>
   );
 };
--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,32 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import { AppRoute, AppRoutes } from "../utils/router";
 import { AuthProvider } from "../hooks/useAuth";
 import { withTheme } from "../hooks/withTheme";
-import Box from "@mui/material/Box";
-import Nav from "../components/Layout/Nav";
-import Footer from "../components/Layout/Footer";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
-        <Box sx={{ display: "flex", height: "100vh" }}>
-          <Nav />
-          <Routes>
-            {AppRoutes.map(({ path, privateRoute, Component }, index) => (
-              <Route
-                key={index}
-                path={path}
-                element={
-                  <AppRoute
-                    Component={<Component />}
-                    privateRoute={privateRoute}
-                  />
-                }
-              />
-            ))}
-          </Routes>
-          <Footer />
-        </Box>
+        <Routes>
+          {AppRoutes.map(({ path, privateRoute, Component }, index) => (
+            <Route
+              key={index}
+              path={path}
+              element={
+                <AppRoute
+                  Component={<Component />}
+                  privateRoute={privateRoute}
+                />
+              }
+            />
+          ))}
+        </Routes>
       </AuthProvider>
     </div>
   );
